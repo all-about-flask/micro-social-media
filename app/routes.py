f947b087818458c100676e0cbd96e3af14166ce1
dzikr <dzikrikovalesky12@gmail.com>
2022-10-06 14:57:59 +0700

    Generating Links

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from . import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 
@@ -25,5 +25,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login Requested for user{}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect('/home')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
