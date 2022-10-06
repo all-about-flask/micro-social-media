86abfde024fca3bd06383856fb084161ad1515ea
dzikr <dzikrikovalesky12@gmail.com>
2022-10-06 14:12:00 +0700

    User login form

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from . import app
 from flask import render_template
+from app.forms import LoginForm
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/home')
 def index():
     user = {'username': 'mdkovalesky'}
     posts = [
@@ -17,3 +18,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
